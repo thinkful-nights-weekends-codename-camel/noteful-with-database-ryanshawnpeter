5716d650dd3af122b55a91232d5f87bec8136428
Peter Tardif <peter.tardif@gmail.com>
2019-07-21 10:40:07 -0600

    adding database to heroku

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
+    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful-database',
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-database-test'
   }
