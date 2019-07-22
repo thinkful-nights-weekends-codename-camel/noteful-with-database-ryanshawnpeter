7c432df048acfa4bd8402948b9bb3a959ca3eaf8
ryan6921 <rmkaw@asu.edu>
2019-07-22 08:32:58 -0700

    added API endpoint

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'production',
     DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/noteful-database',
-    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-database-test'
+    TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://dunder-mifflin@localhost/noteful-database-test',
+    API_ENDPOINT: 'https://shrouded-falls-76226.herokuapp.com/'
   }
